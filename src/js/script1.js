aeefa637a28abe3c0895fccfd9d5a4fd80892d63
Skymen <iskymen@gmail.com>
2016-10-26 00:41:24 +0300

    fix ie8 bugs

--- src/js/script1.js
+++ src/js/script1.js
@@ -1,59 +1,12 @@
-$(document).ready(function () {
-  $('.search__button').on('click', search);
-  $('#search__box').keypress(function (e) {
-    if (e.which == 13) {
-      search();
-    }
-  });
-});
+imageToBackground();
 
 $(document).ready(function () {
-
   $('.header').parallax({imageSrc: 'css/img/header.jpg'});
-
-  $('.slick').slick({
-    arrows: true,
-    dots: false,
-    infinite: true,
-    autoplay: false,
-    speed: 500,
-    cssEase: 'linear'
-  });
-
+  initializeSlider();
+  searchButtonHandler();
   search();
-  console.log($.fn.jquery);
-
+  masonryReload();
 });
-
-$('.slick__slide').each(function () {
-  var src = $(this).find('img').attr('src');
-  $(this).css('background-image', 'url(' + src + ')');
-  $(this).find('img').remove();
-});
-
-$(document).ready(function () {
-
-  if (!(document.documentMode < 9)) {
-    $('.slick__slide').each(function () {
-      var src = $(this).find('img').attr('src');
-      $(this).css('background-image', 'url(' + src + ')');
-      $(this).find('img').remove();
-    });
-  }
-
-  $(function () {
-    // initialize Masonry
-    var container = $('.grid').masonry();
-    // layout Masonry again after all images have loaded
-    container.imagesLoaded(function () {
-      container.masonry();
-    });
-
-
-  });
-
-});
-
 
 function search() {
   var randomImage = [
@@ -72,7 +25,6 @@ function search() {
   if (!(searchString.length > 2)) {
     searchString = randomImage[Math.floor(Math.random() * randomImage.length)];
   }
-
   var searchType;
   if (searchString.indexOf("*") != -1) {
     searchString = searchString.replace('*', "");
@@ -82,15 +34,14 @@ function search() {
     searchType = false;
   }
   searchString = searchString.replace('* ', "");
+  var URL = "http://pixabay.com/api/?key=3585748-15edbd2122b05ecafa57ca9f4&per_page=7&callback=?&q=" + encodeURIComponent(searchString);
 
-  var URL = "https://pixabay.com/api/?key=3585748-15edbd2122b05ecafa57ca9f4&per_page=7&q=" + encodeURIComponent(searchString);
   $.getJSON(URL, function (data) {
     if (parseInt(data.totalHits) > 0) {
       gridArray = $('.grid-content');
       $.each(data.hits, function (i, hit) {
         $(gridArray[i]).html('<div>'+hit.tags+'</div>');
         if (searchType == true) {
-
           $(gridArray[i]).find('img').remove();
           $(gridArray[i])
             .removeClass("grid-height")
@@ -98,10 +49,8 @@ function search() {
           $(gridArray[i]).parent('div').removeClass("grid-item--width2");
           $(gridArray[i])
             .prepend('<img class="grid-image" src="' + hit.webformatURL + '" />');
-
         }
         else {
-
           $(gridArray[i]).find('img').remove();
           $(gridArray[i])
             .css('background-image', 'url(' + hit.webformatURL + ')')
@@ -110,26 +59,16 @@ function search() {
             $(gridArray[i]).parent('div').addClass('grid-item--width2');
           }
         }
-
       });
+      masonryReload();
     }
     else {
       console.log('No results');
     }
-
-    masornyReload();
-
-//    $('.grid').isotope({
-//      // options
-//      itemSelector: '.grid-item',
-//      layoutMode: 'masonry'
-//    });
-
-
   });
 }
 
-function masornyReload() {
+function masonryReload() {
   var $grid = $('.grid').masonry({
     itemSelector: '.grid-item',
     columnWidth: '.grid-item',
@@ -137,5 +76,35 @@ function masornyReload() {
   });
   $grid.imagesLoaded().progress(function () {
     $grid.masonry('layout');
+  });
+}
+
+function imageToBackground() {
+  if (!(document.documentMode < 9)) {
+    $('.slick__slide').each(function () {
+      var src = $(this).find('img').attr('src');
+      $(this).css('background-image', 'url(' + src + ')');
+      $(this).find('img').remove();
+    });
+  }
+}
+
+function searchButtonHandler() {
+  $('.search__button').on('click', search);
+  $('#search__box').keypress(function (e) {
+    if (e.which == 13) {
+      search();
+    }
+  });
+}
+
+function initializeSlider() {
+  $('.slick').slick({
+    arrows: true,
+    dots: false,
+    infinite: true,
+    autoplay: false,
+    speed: 500,
+    cssEase: 'linear'
   });
 }
